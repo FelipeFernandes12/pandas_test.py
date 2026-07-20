c926a3b22d29488a6b0e0eac3af3c29920ac551d
FelipeFernandes12 <felipez12ab@gmail.com>
2026-07-20 11:03:05 -0300

    Changees in my code

--- main.py
+++ main.py
@@ -3,16 +3,55 @@ from pathlib import Path
 import pandas as pd
 
 from src1.cleaning import clean_data
-from src1.analysis import calculate_profit
+from src1.analysis import (
+    calculate_profit,
+    monthly_profit,
+    category_summary
+)
 
 
 base_dir = Path(__file__).resolve().parent
-csv_path = base_dir / "Data" / "Finance2025 - Dados 2025.csv"
+
+csv_path = (
+    base_dir
+    / "Data"
+    / "Finance2025 - Dados 2025.csv"
+)
 
 
+# Load data
 df = pd.read_csv(csv_path)
+
+
+# Cleaning
 df = clean_data(df)
+
+
+# Analysis
 
 profit = calculate_profit(df)
 
-print(f"Lucro total: {profit:.2f}")
+monthly = monthly_profit(df)
+
+summary = category_summary(df)
+
+
+print("--------------------")
+print(f"Total Profit: R$ {profit:.2f}")
+
+
+print("\nMonthly Profit")
+print(monthly)
+
+
+print("\nCategory Summary")
+print(summary)
+
+monthly.to_csv(
+    "outputs/monthly_profit.csv"
+)
+
+
+summary.to_csv(
+    "outputs/financial_summary.csv"
+)
--- src1/analysis.py
+++ src1/analysis.py
@@ -13,3 +13,24 @@ def calculate_profit(df):
     ].sum()
 
     return revenue - expenses
+
+def monthly_profit(df):
+
+    result = (
+        df.groupby("Mes")
+        .apply(calculate_profit)
+    )
+
+    return result
+
+def category_summary(df):
+
+    summary = (
+        df.groupby(
+            ["Categoria", "Natureza financeira"]
+        )["Valor"]
+        .sum()
+        .unstack(fill_value=0)
+    )
+
+    return summary
